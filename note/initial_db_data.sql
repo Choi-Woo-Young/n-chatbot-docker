8184fa3ba811a43d1c7262393d5657533dc66be5
wychoi <success7895@gmail.com>
2025-04-09 18:20:21 +0900

    개발환경 구성을 위한 설정 변경

--- note/initial_db_data.sql
+++ note/initial_db_data.sql
@@ -26,7 +26,7 @@ INSERT INTO code (code_id,code_group_id,code_name,code_desc,code_value,code_orde
 
  INSERT INTO public.embedding_file
 (file_id, file_path, file_name, orign_file_name, category_name, service_cd, service_name, embedding_yn, created_by, created_time, modified_by, last_modified_time, delete_yn, service_desc)
-VALUES(1, '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '법률', 'LAW_001', '신용정보의 이용 및 보호에 관한 법률', false, NULL, '2025-04-08 16:28:17.723', NULL, '2024-09-25 11:21:54.330', false, NULL);
+VALUES(1, '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '신용정보의 이용 및 보호에 관한 법률(법률)(제20304호)(20240814).pdf', '법률', 'LAW', '신용정보의 이용 및 보호에 관한 법률', false, NULL, '2025-04-08 16:28:17.723', NULL, '2024-09-25 11:21:54.330', false, NULL);
 
 INSERT INTO "user" (user_nm,email,"password",role_cd,created_by,created_time,modified_by,last_modified_time,delete_yn,emp_no,mng_services,ip,dept_nm,position_nm,guide_tour_json) VALUES
 	 ('내규봇','nice-law@niceinfo.co.kr','1234','BOT',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'::ffff:10.97.192.101','서비스운영실','매니저','{"login":"N", "chatList":"N", "llmChatPannel":"N"}'),
