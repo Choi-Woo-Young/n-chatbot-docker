54dbb1694cccadb5e6622664a594e65bab947782
wychoi <success7895@gmail.com>
2025-04-09 19:31:02 +0900

    도커 이미지 빌드 시 용량부족 이슈로 --no-cache 설정 추가

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,10 +4,10 @@ set -e
 cd /data/n-chatbot-docker
 
 echo "Building nchatbot-front image..."
-docker build -f ./docker/Dockerfile-front -t nchatbot-front:0.1 .
+docker build --no-cache -f ./docker/Dockerfile-front -t nchatbot-front:0.1 .
 
 echo "Building nchatbot-api image..."
-docker build -f ./docker/Dockerfile-api -t nchatbot-api:0.1 .
+docker build --no-cache -f ./docker/Dockerfile-api -t nchatbot-api:0.1 .
 
 echo "Starting containers with docker compose..."
 docker compose up --build --watch
